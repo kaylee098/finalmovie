bf1505d7636fa04ba2446500f5681ae8075d7461
Kaylee Martin <kayleemartin098@gmail.com>
2022-12-08 22:02:06 -0600

    aincommit

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,38 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    
+    <NavBar></NavBar>
+    <div class="row row-cols-1 row-cols-md-2 g-4">
+        <CardComponent class="col"  v-for="show in tvshows" v-bind:key="show.id" :movie="show"></CardComponent>
+    </div>
+  
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import 'bootstrap'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import CardComponent from './components/CardComponent.vue';
+import NavBar from './components/NavBar.vue'
+import axios from 'axios'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    NavBar,
+    CardComponent
+  },
+  data(){
+    return{
+      tvshows:[]
+    }
+  },
+  mounted(){
+    axios
+    .get("https://api.themoviedb.org/3/movie/upcoming?api_key=70ef7c62eee1244489c96681175a2a0f&language=en-US&page=1")
+    .then((response) => {
+      this.tvshows = response.data.results.slice(0,4);
+    })
   }
 }
 </script>
@@ -21,8 +42,8 @@ export default {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
+
   color: #2c3e50;
-  margin-top: 60px;
+  margin-top: 0px;
 }
 </style>
